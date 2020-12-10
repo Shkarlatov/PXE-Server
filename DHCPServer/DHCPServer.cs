ccc0df185992c0e9eb03b5532200dc6fed150c21
CrazyAlex25 <crazyalex25@mail.ru>
2020-12-10 17:23:24 +0400

    Edit for processing dhcp message

--- DHCPServer/DHCPServer.cs
+++ DHCPServer/DHCPServer.cs
@@ -429,8 +429,13 @@ namespace GitHub.JPMikkers.DHCP
             m_Socket.Send(endPoint, new ArraySegment<byte>(m.ToArray()));
         }
 
+        protected virtual void ProcessingReceiveMessage(DHCPMessage sourceMsg, DHCPMessage targetMsg)
+        {
+
+        }
         private void AppendConfiguredOptions(DHCPMessage sourceMsg,DHCPMessage targetMsg)
         {
+            
             foreach (OptionItem optionItem in m_Options)
             {
                 if (optionItem.Mode == OptionMode.Force || sourceMsg.IsRequestedParameter(optionItem.Option.OptionType))
@@ -441,6 +446,8 @@ namespace GitHub.JPMikkers.DHCP
                     }
                 }
             }
+
+            ProcessingReceiveMessage(sourceMsg, targetMsg);
         }
 
         private void SendOFFER(DHCPMessage sourceMsg, IPAddress offeredAddress, TimeSpan leaseTime)
